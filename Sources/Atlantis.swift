67d028fca074aaf4bf0ea5f857b7d2327142ef9a
Nghia Tran <vinhnghiatran@gmail.com>
2020-10-22 21:56:21 +0700

    First attempt to swizzle URLSessionTask.resume

--- Sources/Atlantis.swift
+++ Sources/Atlantis.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import ObjectiveC
 
 /// Inspire from Flex
 /// https://github.com/FLEXTool/FLEX/tree/master/Classes/Network/PonyDebugger
@@ -36,8 +37,54 @@ final class Atlantis: NSObject {
 extension Atlantis {
 
     private class func injectAllNetworkClasses() {
-        DispatchQueue.once {
+        injectURLSessionResume()
+    }
+}
 
+// MARK: - URLSession
+
+extension Atlantis {
+
+    private class func injectURLSessionResume() {
+        // In iOS 7 resume lives in __NSCFLocalSessionTask
+        // In iOS 8 resume lives in NSURLSessionTask
+        // In iOS 9 resume lives in __NSCFURLSessionTask
+        // In iOS 14 resume lives in NSURLSessionTask
+        var baseResumeClass: AnyClass? = nil;
+        if ProcessInfo.processInfo.responds(to: #selector(getter: ProcessInfo.operatingSystemVersion)) {
+            baseResumeClass = NSClassFromString("__NSCFLocalSessionTask")
+        } else {
+            let majorVersion = ProcessInfo.processInfo.operatingSystemVersion.majorVersion
+            if majorVersion < 9 || majorVersion >= 14 {
+                baseResumeClass = URLSessionTask.self
+
+            } else {
+                baseResumeClass = NSClassFromString("__NSCFURLSessionTask")
+            }
         }
+
+        guard let resumeClass = baseResumeClass else {
+            assertionFailure()
+            return
+        }
+
+        _swizzleResumeSelector(baseClass: resumeClass)
+    }
+
+    private class func _swizzleResumeSelector(baseClass: AnyClass) {
+
+        let selector = NSSelectorFromString("resume")
+        guard let method = class_getInstanceMethod(baseClass, selector),
+            baseClass.instancesRespond(to: selector) else {
+            assertionFailure()
+            return
+        }
+
+        let originalIMP = method_getImplementation(method)
+        let swizzleIMP = imp_implementationWithBlock({ (self: URLSessionTask) -> Void in
+            let oldIMP = unsafeBitCast(originalIMP, to: (@convention(c) (URLSessionTask, Selector) -> Void).self)
+            oldIMP(self, selector)
+            } as @convention(block) (URLSessionTask) -> Void)
+        method_setImplementation(method, swizzleIMP)
     }
 }
