bbff9411c1ed7fa74b0cd0b26b5fe73980b0f871
Nghia Tran <vinhnghiatran@gmail.com>
2020-10-27 13:35:51 +0700

    Fix SPM for iOS 12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Atlantis",
     platforms: [.macOS(.v10_12),
-                .iOS(.v13)],
+                .iOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
