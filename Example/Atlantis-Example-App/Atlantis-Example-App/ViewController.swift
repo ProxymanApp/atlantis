22a2e2de6909386fdf6b1d9068f7302e1b6c9160
Nghia Tran <vinhnghiatran@gmail.com>
2021-10-23 10:37:51 +0700

    Complete a simple example app

--- Example/Atlantis-Example-App/Atlantis-Example-App/ViewController.swift
+++ Example/Atlantis-Example-App/Atlantis-Example-App/ViewController.swift
@@ -29,13 +29,46 @@ final class ViewController: UIViewController {
     }
 
     @IBAction func postJsonBtnOnTap(_ sender: Any) {
+        let url = URL(string: "https://httpbin.org/post")!
+        let body: [String: String] = ["name": "Proxyman",
+                                       "id": "123"]
+        let header: [String: String] = ["X-Proxyman-Data": "123",
+                                        "X-Data": "JSON"]
+        AF.request(url, method: HTTPMethod.post, parameters: body, encoder: JSONParameterEncoder(), headers: HTTPHeaders(header)).responseJSON { response in
+            print(response)
+        }
     }
 
     @IBAction func postUrlEncodedBtnOnTap(_ sender: Any) {
+        let url = URL(string: "https://httpbin.org/post")!
+        let body: [String: String] = ["name": "Proxyman",
+                                       "id": "123"]
+        let header: [String: String] = ["X-Proxyman-Data": "123",
+                                        "X-Data": "JSON"]
+        AF.request(url, method: HTTPMethod.post, parameters: body, encoder: URLEncodedFormParameterEncoder(), headers: HTTPHeaders(header)).responseJSON { response in
+            print(response)
+        }
     }
+
     @IBAction func uploadMultipartFormBtnOnClick(_ sender: Any) {
+        let imageURL = Bundle.main.url(forResource: "image", withExtension: "jpg")!
+        let url = URL(string: "https://httpbin.org/post")!
+        let data = MultipartFormData()
+        data.append("Hello Word".data(using: .utf8)!, withName: "text")
+        data.append(imageURL, withName: "image", fileName: "bigsur", mimeType: "jpg")
+        AF.upload(multipartFormData: data, to: url).responseJSON { response in
+            print(response)
+        }
     }
 
     @IBAction func deleteBtnOnTap(_ sender: Any) {
+        let url = URL(string: "https://httpbin.org/delete")!
+        let body: [String: String] = ["name": "Proxyman",
+                                       "id": "123"]
+        let header: [String: String] = ["X-Proxyman-Data": "123",
+                                        "X-Data": "JSON"]
+        AF.request(url, method: HTTPMethod.delete, parameters: body, encoder: JSONParameterEncoder(), headers: HTTPHeaders(header)).responseJSON { response in
+            print(response)
+        }
     }
 }
