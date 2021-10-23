ded78bc636fef828d82f9f2f615046303baa88c8
Nghia Tran <vinhnghiatran@gmail.com>
2021-10-23 09:27:13 +0700

    Init new Example project

---
+++ Example/Atlantis-Example-App/Atlantis-Example-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atlantis-Example-App
+//
+//  Created by Nghia Tran on 23/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
