91c61b0d45af144a52197dd521e0fd9286d56332
Yi Ding <yi.ding.o@nio.com>
2021-11-30 01:20:13 +0800

    Fix Swift Package dependencies.

--- Package.swift
+++ Package.swift
@@ -18,12 +18,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Atlantis-Objc",
-            path: "Objc",
-            publicHeadersPath: "internal"),
-        .target(
             name: "Atlantis",
-            dependencies: ["Atlantis-Objc"],
+            dependencies: [],
             path: "Sources")
     ],
     swiftLanguageVersions: [.v5]
