b32c0e319a97b213a2eff24bab5134d7152ceb89
Nghia Tran <vinhnghiatran@gmail.com>
2025-04-19 12:58:49 +0700

    Update AtlantisSwiftUIAppApp.swift

--- Example/AtlantisSwiftUIApp/AtlantisSwiftUIApp/AtlantisSwiftUIAppApp.swift
+++ Example/AtlantisSwiftUIApp/AtlantisSwiftUIApp/AtlantisSwiftUIAppApp.swift
@@ -18,13 +18,13 @@ struct AtlantisSwiftUIAppApp: App {
     init() {
         // 2. Connect to your Macbook
         #if DEBUG
-//        Atlantis.start()
+        Atlantis.start()
         
         // 3. (Optional)
         // If you have many Macbooks on the same WiFi Network, you can specify your Macbook's name
         // Find your Macbook's name by opening Proxyman App -> Certificate Menu -> Install Certificate for iOS -> With Atlantis ->
         // Click on "How to start Atlantis" -> Select "SwiftUI" Tab
-        Atlantis.start(hostName: "mac-mini.local.")
+        // Atlantis.start("Your's Macbook Pro")
         #endif
     }
 
